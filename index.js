08ba0891e006480fe31086fbad6ac76ff03b315b
EgertL123 <linnasegert@gmail.com>
2025-09-18 13:49:28 +0300

    Created connection to database refactor

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+dotenv.config();
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize')
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+});
 
     sequelize
         .authenticate()
         .then(() => {
-            console.log('Connection has been established successfully.');
+            console.log('Connection to database has been established successfully.');
         })
         .catch(err => {
             console.error('Unable to connect to the database:', err);
