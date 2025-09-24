2a511e1cd9d019e18ec8b7e1a961804961395b6f
EgertL123 <linnasegert@gmail.com>
2025-09-24 12:06:39 +0300

    Created author table and added table data

---
+++ migrations/20250924080510-modify_author_fields.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return Promise.all([
+      queryInterface.changeColumn('Authors', 'id', {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+    }),
+     queryInterface.changeColumn('Authors', 'name', {
+      type: Sequelize.STRING,
+      allowNull: false
+    }),
+  ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    return Promise.all([
+    queryInterface.dropTable('Authors')
+    ])
+  }
+};
---
+++ seeders/20250924061226-demo-articles.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return Promise.all([
+        queryInterface.bulkInsert('Articles', [{
+          name: 'Introduction to Ashtanga',
+          slug: 'introduction-to-ashtanga',
+          image: 'ashtanga.jpg',
+          body: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, ut, voluptatibus. Aliquid aspernatur blanditiis eos fuga iste itaque molestias pariatur reiciendis rem sed. Laborum nisi nostrum recusandae reprehenderit similique sunt?</p>',
+          published: '2020-01-08 15:02:30',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }]),
+        queryInterface.bulkInsert('Articles', [{
+          name: 'Morning vinyasa flow routine',
+          slug: 'morning-vinyasa-flow-routine',
+          image: 'morning.jpg',
+          body: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias cum dignissimos dolore ea eaque ipsum neque quis quod. Ad aliquid doloremque eaque facere, incidunt laboriosam magnam numquam pariatur quibusdam quo!</p>',
+          published: '2020-04-14 15:02:41',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }]),
+        queryInterface.bulkInsert('Articles', [{
+          name: 'Secrets of a yoga teacher',
+          slug: 'secrets-of-a-yoga-teacher',
+          image: 'yoga-teacher.jpg',
+          body: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, consequatur cumque delectus est eveniet excepturi facere fugit ipsum minima nam nemo numquam pariatur quae quasi quis sapiente similique sunt veritatis?</p>',
+          published: '2060-05-14 15:02:55',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }])
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+  }
+};
