423d02b71d7ec94eaca06638c1c2c3f27a599512
EgertL123 <linnasegert@gmail.com>
2025-10-05 14:49:14 +0300

    Get article data by slug

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
 
 const articleRouter = require('./routers/article');
 app.use('/', articleRouter);
+app.use('/article', articleRouter);
 
 app.listen(3000, () => {
     console.log("Server is running on http://localhost:3000");
